63886b8e576617aba3d551d8385845b9ff8360db
Hea-Rith <hearith9099@gmail.com>
2026-07-03 14:13:19 +0700

    ChatSystem — Email verification frontend

--- vuejs-app/src/router/index.js
+++ vuejs-app/src/router/index.js
@@ -1,6 +1,7 @@
 import Signin from '@/components/auth/Signin.vue';
 import Signout from '@/components/auth/Signout.vue';
 import Signup from '@/components/auth/Signup.vue';
+import VerifyEmail from '@/components/auth/VerifyEmail.vue';
 import Dashboard from '@/components/pages/Dashboard.vue';
 import { createRouter, createWebHistory } from 'vue-router';
 
@@ -26,6 +27,13 @@ const router = createRouter({
       component: Signup,
       meta: { guarded: false },
     },
+    {
+      path: '/verify/email',
+      name: 'auth.verify.email',
+      component: VerifyEmail,
+      meta: { guarded: false },
+    },
+    
     {
       path: '/dashboard',
       name: 'dashboard',
